67a6b3e55cc32a1845c800427050d882033d9b5e
Felipe Limeira <limeira.felipe94@gmail.com>
2023-02-21 17:39:09 -0300

    adiconado GEOJSON

---
+++ Reamd.md
@@ -0,0 +1,1 @@
+### Escrevendo a documentação 
---
+++ data/points.js
@@ -0,0 +1,60 @@
+var pointsJSON = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -46.55994346824741,
+                    -23.708570176779446
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -46.649664309266456,
+                    -23.5473331586701
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -46.784245570794496,
+                    -23.53705152950397
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -46.51853384931542,
+                    -23.449229532811827
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -46.57073527556091,
+                    -23.624018666121387
+                ],
+                "type": "Point"
+            }
+        }
+    ]
+}
